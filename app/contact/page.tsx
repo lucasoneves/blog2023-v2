64a979ac4a5d98e1a99e9a64581938eb8c125b41
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-09 18:28:16 -0300

    Contact Form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Contact() {
-  return <h2>Contact me</h2>
+  const [email, setEmail] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await fetch('/api/contact', {
+      method: "POST",
+      body: JSON.stringify({email}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+
+    console.log('het')
+
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
